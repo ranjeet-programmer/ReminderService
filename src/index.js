82aeed558effadb2433907d61be74a44966b5d22
ranjeet-programmer <ranjeethingeofficial@gmail.com>
2023-03-07 14:17:32 +0530

    basic reminder service skeleton is done

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const { PORT } = require("./config/serverConfig");
+
+const setupAndStartServer = () => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.listen(PORT, () => {
+    console.log(` server started on port ${PORT}`);
+  });
+};
+
+setupAndStartServer();
