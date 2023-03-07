d67b4d3bef10e977b4aedd62f4c07284dae805da
ranjeet-programmer <ranjeethingeofficial@gmail.com>
2023-03-07 21:25:19 +0530

    created database schema

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ const app = express();
 
 const { PORT } = require("./config/serverConfig");
 
-const { sendBasicEmail } = require("./services/email-service");
-
 const setupAndStartServer = () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,13 +12,6 @@ const setupAndStartServer = () => {
   app.listen(PORT, () => {
     console.log(` server started on port ${PORT}`);
   });
-
-  sendBasicEmail(
-    "support@testing.com",
-    "ranjeethingeofficial@gmail.com",
-    "Welcome to the Backend Course",
-    "Please submit a below form so that it will help's to you"
-  );
 };
 
 setupAndStartServer();
--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,12 +1,16 @@
 const sender = require("../config/emailConfig");
 
-const sendBasicEmail = (mailFrom, mailTo, mailSubject, mailBody) => {
-  sender.sendMail({
-    from: mailFrom,
-    to: mailTo,
-    subject: mailSubject,
-    text: mailBody,
-  });
+const sendBasicEmail = async (mailFrom, mailTo, mailSubject, mailBody) => {
+  try {
+    const response = await sender.sendMail({
+      from: mailFrom,
+      to: mailTo,
+      subject: mailSubject,
+      text: mailBody,
+    });
+  } catch (error) {
+    console.log("Gmail not sent", error);
+  }
 };
 
 module.exports = {
