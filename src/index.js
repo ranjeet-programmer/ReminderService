88c027b877893cbce095f1e55756133c0468cb17
ranjeet-programmer <ranjeethingeofficial@gmail.com>
2023-03-07 15:34:16 +0530

    done with basic functionality that will send the mail

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const app = express();
 
 const { PORT } = require("./config/serverConfig");
 
+const { sendBasicEmail } = require("./services/email-service");
+
 const setupAndStartServer = () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,6 +14,13 @@ const setupAndStartServer = () => {
   app.listen(PORT, () => {
     console.log(` server started on port ${PORT}`);
   });
+
+  sendBasicEmail(
+    "support@testing.com",
+    "ranjeethingeofficial@gmail.com",
+    "Welcome to the Backend Course",
+    "Please submit a below form so that it will help's to you"
+  );
 };
 
 setupAndStartServer();
